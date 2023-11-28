fa75016a3b8f5f50fe1b76b089c192bc1ca77f5f
Putri Indriyanti <putri.indriyanti21@students.unila.ac.id>
2023-11-28 12:06:18 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { PORT = 3000} = process.env;
+
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const userRouter = require('./routes/user.routes');
+app.use('/auth', userRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}`);
+});
