64cdaab09ac8c3e2db8ef4be5222570926f5f05e
asephudori <asep.hudori532@gmail.com>
2023-11-29 14:35:07 +0700

    add verify otp and resend otp

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-const { register, login, updateOtp } = require('../controllers/users.controllers');
+const { verify } = require('jsonwebtoken');
+const { register, login, verifyOtp, resendOtp , updateOtp } = require('../controllers/users.controllers');
 const {  restrict } = require('../middlewares/auth.middlewares');
 
 router.post('/register', register);
+router.post('/verify-otp', verifyOtp)
+router.post('/resend-otp', resendOtp)
 router.post('/login', login);
 
 
