6a4930f77411f646ca4558f1dff2ed6ce7cde231
putriindriyanti <90454501+putriindriyanti@users.noreply.github.com>
2023-12-01 13:42:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const { PORT = 3000} = process.env;
 const morgan = require('morgan')
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const userRouter = require('./routes/user.routes');
 const profileRouter = require('./routes/profile.routes')
