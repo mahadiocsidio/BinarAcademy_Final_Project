51979eb151dff8d32e40dfa0a5aad3740a9ed336
mahadiocsidio <mahadiocsidio@students.amikom.ac.id>
2023-12-06 00:45:05 +0700

    profile editing and docs

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const {restrict} = require('../middlewares/auth.middlewares')
-const {getAllAccount,getAccountbyId,updateProfile,changePassword,logout} = require('../controllers/profile.controllers')
+const {getAllAccountProfile,getAccountbyId,updateProfile,changePassword,logout} = require('../controllers/profile.controllers')
 
-router.get('/', getAllAccount)
-router.get('/:account_id',getAccountbyId)
+router.get('/', getAllAccountProfile)
+router.get('/account',restrict, getAccountbyId)
 router.put('/updateProfile',restrict, updateProfile)
 router.put('/changePassword',restrict,changePassword)
 router.post('/logout',logout)
